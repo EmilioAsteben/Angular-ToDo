4afa3a67698c0938ffdca419cca789a341dcf484
Emilio <asteben@bk.ru>
2021-09-30 00:29:29 +0300

    'env-prod'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://intense-taiga-29233.herokuapp.com/',
+  geturl() {
+    return `${this.apiUrl}/projects`;
+  },
+  postUrl() {
+    return `${this.apiUrl}/todos`;
+  },
+  patchUrl(project_id: number, todo_id: number) {
+    return `${this.apiUrl}/projects/${project_id}/todo/${todo_id}`;
+  },
 };
